818d99355300dccbb09a07cb565056cb035b806d
Jhe0031 <jackie.he8@gmail.com>
2018-09-06 09:02:45 -0400

    3 functions

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -8,3 +8,22 @@ def repeat_string(word):
 print(greeter"Stan"))
 print(greeter("Ollie"))
 print(repeat_string("hello))
+                    
+def hello_name(name):
+  return "Hello " + name + "!"
+print (hello_name('Bob'))
+print (hello_name('Alice'))
+print (hello_name('X'))
+
+def make_abba(a, b):
+  return a + b + b + a
+print (make_abba('Hi', 'Bye') → 'HiByeByeHi')
+print (make_abba('Yo', 'Alice') → 'YoAliceAliceYo')
+print (make_abba('What', 'Up') → 'WhatUpUpWhat')
+                    
+def make_tags(tag, word):
+  return "<" +  tag + ">" + word + "</" + tag + ">"
+print (make_tags('i', 'Yay') → '<i>Yay</i>')
+print (make_tags('i', 'Hello') → '<i>Hello</i>')
+print (make_tags('cite', 'Yay') → '<cite>Yay</cite>')
+                    
