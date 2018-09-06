f479f6dc8751c7ab8a9c539560f95e7718b24235
Jhe0031 <jackie.he8@gmail.com>
2018-09-06 08:38:14 -0400

    adding all files

---
+++ lab_01/program1.py
@@ -0,0 +1,10 @@
+def greeter(name):
+    return "Hello " + name + "!"
+print(greeter("Stan"))
+
+def repeat_string(word):
+    return word+word
+
+print(greeter"Stan"))
+print(greeter("Ollie"))
+print(repeat_string("hello))
