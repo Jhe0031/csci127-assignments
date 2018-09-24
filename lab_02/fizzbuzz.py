d6ecec285e55b49046446b4223ebbfa0191c2649
Jhe0031 <jackie.he8@gmail.com>
2018-09-24 08:39:23 -0400

    Updated lab

--- lab_02/fizzbuzz.py
+++ lab_02/fizzbuzz.py
@@ -1,23 +1,20 @@
 #Partners: Christopher He & Jia Qi He
 
-i = 1
-fizz = 0
-buzz = 0
-fizzbuzz = 0
-while  i <= 100:
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
-        fizzbuzz = fizzbuzz + 1
-    elif i % 3 == 0:
-        print("fizz")
-        fizz = fizz + 1
-    elif i % 5 == 0:
-        print("buzz")
-        buzz = buzz + 1
-    else:
-        print(i)
-    i = i + 1
-print("fizzbuzz done!")
-print("# of fizz = " + str(fizz))
-print("# of buzz = " + str(buzz))
-print("# of fizzbuzz = " + str(fizzbuzz))
+def fizzbuzz(max_value):
+    i = 1
+    fizzbuzz = 0
+    while  i <= max_value:
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+            fizzbuzz = fizzbuzz + 1
+        elif i % 3 == 0:
+            print("fizz")
+            fizz = fizz + 1
+        elif i % 5 == 0:
+            print("buzz")
+            buzz = buzz + 1
+        else:
+            print(i)
+        i = i + 1
+    return(fizzbuzz)
+print("# of 'fizzbuzz' = " + str(fizzbuzz(100)))
