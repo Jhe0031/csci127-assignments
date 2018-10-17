2693157238e1a70e44afc1bf1ccfc57c3c61c1d0
Jhe0031 <jackie.he8@gmail.com>
2018-10-16 22:07:40 -0400

    Updated story

--- lab_03/madlibs.py
+++ lab_03/madlibs.py
@@ -1,10 +1,17 @@
 # Lauren & Jia Qi He
 
-adjective_list = []
-adverb_list = []
+import random
+
+title = "A CHARMING STORY WITH A HAPPY ENDING"
+story = "Once upon a/an %NOUN% there were three little pigs. The first little pig was very %ADJECTIVE% and he built a house for himself out of %PNOUN%. The second pig was %ADJECTIVE% and he built a house out of %PNOUN%. But the third little pig was very %ADJECTIVE% and he built his house out of genuine %PNOUN%. Well, one day, a mean old wolf came along and saw the houses. '%SOUND%!' he said, 'I'll %VERB% and I'll %VERB% and I'll blow your house down.' And he blew down the first pig's %NOUN% and the second pig's %NOUN%. The two pigs ran to the third pig's house. Thereupon, the wolf began blowing, but he couldn't blow down the third pig's %ADJECTIVE% house. So he %VERB_PAST% off into the forest and the three %ADJECTIVE% pigs moved to Chicago and went into the sausage business."
 noun_list = []
+adj_list = []
+pnoun_list = []
+sound_list = []
 verb_list = []
-name_list = []
+verb_past_list = []
 
-
+def madlibs():
+    
+    
 print()
