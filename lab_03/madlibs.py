5a6b4c7f24ed71deaa686528f9b5bd6107699569
Jhe0031 <jackie.he8@gmail.com>
2018-10-17 15:43:19 -0400

    Added objects to arrays

--- lab_03/madlibs.py
+++ lab_03/madlibs.py
@@ -3,14 +3,14 @@
 import random
 
 title = "A CHARMING STORY WITH A HAPPY ENDING"
-story = "Once upon a/an time there were three little %ANIMAL%. The first little %ANIMAL% was very %ADJECTIVE% and he built a house for himself out of %PNOUN%. The second %ANIMAL% was %ADJECTIVE% and he built a house out of %PNOUN%. But the third little %ANIMAL% was very %ADJECTIVE% and he built his house out of genuine %PNOUN%. Well, one day, a mean old wolf came along and saw the houses. '%SOUND%!' he said, 'I'll %VERB% and I'll %VERB% and I'll blow your house down.' And he blew down the first %ANIMAL%'s %NOUN% and the second %ANIMAL%'s %NOUN%. The two %ANIMAL%s ran to the third %ANIMAL%'s house. Thereupon, the wolf began blowing, but he couldn't blow down the third %ANIMAL%'s %ADJECTIVE% house. So he %VERB_PAST% off into the forest and the three %ADJECTIVE% %ANIMAL%s moved to Chicago and went into the sausage business."
-animal_list = [] #this one doesn't change
-noun_list = []
-adj_list = []
-pnoun_list = []
-sound_list = []
-verb_list = []
-verb_past_list = []
+story = "Once upon a/an time there were three little %ANIMAL%" + "s. The first little %ANIMAL% was very %ADJECTIVE% and he built a house for himself out of %PNOUN%. The second %ANIMAL% was %ADJECTIVE% and he built a house out of %PNOUN%. But the third little %ANIMAL% was very %ADJECTIVE% and he built his house out of genuine %PNOUN%. Well, one day, a mean old wolf came along and saw the houses. '%SOUND%!' he said, 'I'll %VERB% and I'll %VERB% and I'll blow your house down.' And he blew down the first %ANIMAL%" + "'s %NOUN% and the second %ANIMAL%" + "'s %NOUN%. The two %ANIMAL%s ran to the third %ANIMAL%'s house. Thereupon, the wolf began blowing, but he couldn't blow down the third %ANIMAL%'s %ADJECTIVE% house. So he %VERB_PAST% off into the forest and the three %ADJECTIVE% %ANIMAL%s moved to Chicago and went into the sausage business."
+animal_list = ["pig", "dog", "cat", "cow", "armadillo"] #this one doesn't change
+noun_list = ["light" "apple juice", "leg", "pencil", "house"] #nouns
+adj_list = ["freaky", "scary", "funny", "strong", "weak"] #adjectives
+pnoun_list = ["bricks", "socks", "papers", "ice cream tubs", "cards"] #plural nouns
+sound_list = ["woof", "oof", "pft", "awoo", "tch"] #sounds
+verb_list = ["jump", "blow", "throw", "pounce", "break"] #verbs
+verb_past_list = ["jumped", "rolled", "slinked", "ran", "teleported"] #past tense verbs
 
 def madlibs():
     
