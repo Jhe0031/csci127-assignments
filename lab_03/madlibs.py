59a5d38bf91f810f90be61d5959266e81b833d13
Jhe0031 <jackie.he8@gmail.com>
2018-10-16 22:15:33 -0400

    including extra assignment

--- lab_03/madlibs.py
+++ lab_03/madlibs.py
@@ -3,7 +3,8 @@
 import random
 
 title = "A CHARMING STORY WITH A HAPPY ENDING"
-story = "Once upon a/an %NOUN% there were three little pigs. The first little pig was very %ADJECTIVE% and he built a house for himself out of %PNOUN%. The second pig was %ADJECTIVE% and he built a house out of %PNOUN%. But the third little pig was very %ADJECTIVE% and he built his house out of genuine %PNOUN%. Well, one day, a mean old wolf came along and saw the houses. '%SOUND%!' he said, 'I'll %VERB% and I'll %VERB% and I'll blow your house down.' And he blew down the first pig's %NOUN% and the second pig's %NOUN%. The two pigs ran to the third pig's house. Thereupon, the wolf began blowing, but he couldn't blow down the third pig's %ADJECTIVE% house. So he %VERB_PAST% off into the forest and the three %ADJECTIVE% pigs moved to Chicago and went into the sausage business."
+story = "Once upon a/an time there were three little %ANIMAL%. The first little %ANIMAL% was very %ADJECTIVE% and he built a house for himself out of %PNOUN%. The second %ANIMAL% was %ADJECTIVE% and he built a house out of %PNOUN%. But the third little %ANIMAL% was very %ADJECTIVE% and he built his house out of genuine %PNOUN%. Well, one day, a mean old wolf came along and saw the houses. '%SOUND%!' he said, 'I'll %VERB% and I'll %VERB% and I'll blow your house down.' And he blew down the first %ANIMAL%'s %NOUN% and the second %ANIMAL%'s %NOUN%. The two %ANIMAL%s ran to the third %ANIMAL%'s house. Thereupon, the wolf began blowing, but he couldn't blow down the third %ANIMAL%'s %ADJECTIVE% house. So he %VERB_PAST% off into the forest and the three %ADJECTIVE% %ANIMAL%s moved to Chicago and went into the sausage business."
+animal_list = [] #this one doesn't change
 noun_list = []
 adj_list = []
 pnoun_list = []
