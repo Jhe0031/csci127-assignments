96e4feff758b5fb01441e510939ac742c9a64657
Jhe0031 <jackie.he8@gmail.com>
2018-10-18 08:49:13 -0400

    Completed finished program

--- lab_03/madlibs.py
+++ lab_03/madlibs.py
@@ -2,7 +2,6 @@
 
 import random
 
-title = "A CHARMING STORY WITH A HAPPY ENDING"
 story = "Once upon a/an time there were three little <ANIMAL> s. The first little <ANIMAL> was very <ADJECTIVE> and he built a house for himself out of <PNOUN> . The second <ANIMAL> was <ADJECTIVE> and he built a house out of <PNOUN> . But the third little <ANIMAL> was very <ADJECTIVE> and he built his house out of genuine <PNOUN> . Well, one day, a mean old wolf came along and saw the houses. ' <SOUND> !!!' he said, 'I'll <VERB> and I'll <VERB> and I'll blow your house down.' And he blew down the first <ANIMAL> 's <NOUN> and the second <ANIMAL> 's <NOUN> . The two <ANIMAL> s ran to the third <ANIMAL> 's house. Thereupon, the wolf began blowing, but he couldn't blow down the third <ANIMAL> 's <ADJECTIVE> house. So he <PVERB> off into the forest and the three <ADJECTIVE> <ANIMAL> s moved to Chicago and went into the <FOOD> business."
 animal_list = ["pig", "dog", "cat", "cow", "armadillo"] #this one doe n't change
 noun_list = ["light" "apple juice", "leg", "pencil", "house"] #nouns
