d224fa727bd2911d9883333be833a975afc4849c
Jhe0031 <jackie.he8@gmail.com>
2018-10-22 09:12:44 -0400

    Updated cake.py

--- exam_01/cake.py
+++ exam_01/cake.py
@@ -1,12 +1,18 @@
 def divide(A, B, u):
     slice = (A/B)*u
     i = 0
+    even_odd = 0
     while i < 10:
         if i*slice == 1:
+            even_odd += 1
             invitees = i
-            break
         i += 1
-    return invitees
+    if even_odd == 0:
+        return "None. If the cake is not even, there is no party."
+    elif even_odd > 0:
+        return invitees
 print('Invitees:', divide(5, 10, 1))
 print('Invitees:', divide(4, 8, 2))
 print('Invitees:', divide(5, 10, .5))
+print('Invitees:', divide(3, 9, 1))
+print('Invitees:', divide(3, 4, 1))
