d9c29e39186af6fce2e33da19d86ac4c7e66b128
Jhe0031 <jackie.he8@gmail.com>
2018-10-27 19:16:42 -0400

    Completed programs

--- hw_06/stats.py
+++ hw_06/stats.py
@@ -8,8 +8,8 @@ def build_random_list(items, max_value):
     """
     l = []
     while len(l) < items:
-        l.append(random.randint(0, max_value))
-        l = l + [random.randint(0, max_value)]
+        l.append(random.randint(1, max_value))
+        l = l + [random.randint(1, max_value)]
     return l
 l = build_random_list(100, 10)
 print(l)
@@ -22,12 +22,28 @@ def max(l):
     return largest
 print('Largest Number:', max(l))
 
-def freq(l, number):
+def freq(l, val):
     count = 0
     for item in l:
-        if item == number:
+        if item == val:
             count += 1
     return count
 print('Frequency of 10:', freq(l, 10))
+print('Frequency of 9:', freq(l, 9))
+print('Frequency of 8:', freq(l, 8))
+print('Frequency of 7:', freq(l, 7))
+print('Frequency of 6:', freq(l, 6))
 print('Frequency of 5:', freq(l, 5))
+print('Frequency of 4:', freq(l, 4))
 print('Frequency of 3:', freq(l, 3))
+print('Frequency of 2:', freq(l, 2))
+print('Frequency of 1:', freq(l, 1))
+
+def mode(l):
+    most = 0
+    for number in l:
+        if freq(l, number) > most:
+            most = freq(l, number)
+            a_mode = number
+    return a_mode
+print('Mode:', mode(l))
