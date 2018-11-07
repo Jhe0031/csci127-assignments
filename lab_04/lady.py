04ce9571b7b144c103d8e9ccc3eded18b3233ce4
Jhe0031 <jackie.he8@gmail.com>
2018-11-07 16:38:35 -0500

    Updated using dictionaries

--- lab_04/lady.py
+++ lab_04/lady.py
@@ -10,28 +10,39 @@ import sys
 def happyLadybugs(b):
     b = b.upper()
     if "_" not in b:
-        i = -1
+        i = 0
         for letter in b:
-            if letter == b[i+1] or letter == b[i-1]:
-                i += 1
-            elif letter != b[i+1]:
+            if letter == b[i+1]:
+                if i+1 >= len(b)-1:
+                    return "YES"
+                else:
+                    i += 1
+            elif letter == b[i-1] and i < len(b):
+                if i+1 >= len(b)-1:
+                    return "YES"
+                else:
+                    i += 1
+            elif letter != b[i+1] and i < len(b):
                 return "NO"
+            elif i >= len(b):
+                return "YES"
         return "YES"
     elif "_" in b:
         b = b.replace("_", "")
         if len(b) <= 1:
             return "NO"
         else:
+            count = {}
             for letter in b:
+                count[letter] = 0
                 i = 0
-                count = 0
                 while i < len(b):
                     if b[i] == letter:
-                        count += 1
+                        count[letter] = count[letter]+1
                     i += 1
-                if count < 2:
+                if count[letter] < 2:
                     return "NO"
-                elif count >= 2:
+                elif count[letter] >= 2:
                     continue
         return "YES"
 print(happyLadybugs("RBY_YBR"))
