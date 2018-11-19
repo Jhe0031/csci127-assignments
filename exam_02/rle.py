2f8a06d01d452542d15ba9b8073cce258c214b18
Jhe0031 <jackie.he8@gmail.com>
2018-11-19 09:23:36 -0500

    End of exam

---
+++ exam_02/rle.py
@@ -0,0 +1,26 @@
+def encode(s):
+    #Not complete
+    list = []
+    count = 0
+    i = 0
+    while i < len(s)-1:
+        if i == len(s)-2:
+            count += 1
+            list.append('[' + s[i] + ', ' + str(count) + ']')
+        elif s[i] == s[i+1]:
+            count += 1
+        elif s[i] != s[i+1]:
+            count += 1
+            list.append('[' + s[i] + ', ' + str(count) + ']')
+            count = 0
+        i += 1
+    return list
+print(encode("abbaaacddaaa"))
+print(encode("abcd"))
+print(encode("cbbbbbdee"))
+
+##Not complete
+##new_s = []
+##def decode(d):
+##    for letter in d:
+##        d[letter] = 
