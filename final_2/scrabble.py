8e5b670aa2467429f77f55551ec1c20f846d78b0
Jhe0031 <jackie.he8@gmail.com>
2018-12-20 10:07:57 -0500

    Completed part 2

--- final_2/scrabble.py
+++ final_2/scrabble.py
@@ -13,3 +13,27 @@ print(canMakeWord("eerriin", "eerie"))
 print(canMakeWord("orrpgma", "program"))
 print(canMakeWord("orppgma", "program"))
 
+print('-------------')
+
+def withWild(letters, word):
+    lett = []
+    wild = 0
+    for alpha in letters:
+        if alpha == "?":
+            wild += 1
+        else:
+            lett.append(alpha)
+    for alpha in word:
+        if alpha in lett:
+            lett.remove(alpha)
+        else:
+            if wild >= 1:
+                wild -= 1
+            else:
+                return False
+    return True
+print(withWild("ladilmy", "daily"))
+print(withWild("e?erriin", "eerie"))
+print(withWild("orrpgma", "program"))
+print(withWild("orppgma", "program"))
+print(withWild("?????", "apple"))
